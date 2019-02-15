829ea0a8a5567460309b1b5ef4c21befa14b342f
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-15 11:30:56 +0100

    Background section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,49 @@
 import React from "react";
-import { Link } from "gatsby";
-import { FaGulp } from "react-icons/fa";
+import { Link, graphql } from "gatsby";
+
+import BackgroundSection from "../components/Globals/BackgroundSection";
 import Layout from "../components/layout";
 
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <h3>
-      Lets go for a <FaGulp />
-    </h3>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  console.log(data);
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title={"Hello there"}
+        styleClass="default-background"
+      />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+// export const query = graphql`
+//   query {
+//     file(relativePath: { eq: "default-background.jpeg" }) {
+//       childImageSharp {
+//         fluid {
+//           ...GatsbyImageSharpFluid_tracedSVG
+//         }
+//       }
+//     }
+//   }
+// `;
 
 export default IndexPage;
